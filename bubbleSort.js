5c538979b854c49bea4f32d1919c1e9392d7caa0
Gaurav <grvjain56@gmail.com>
2022-10-16 20:30:07 +0530

    Selection Sort Completed

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,12 +1,10 @@
 async function bubble()
 {
-    // console.log('In bubbe()') ;
     let ele = document.querySelectorAll(".bar") ;
     for(let i = 0 ; i < ele.length - 1 ; i++)
     {
         for(let j = 0 ; j < ele.length-i-1 ; j++)
         {
-            // console.log('In jth loop');
             ele[j].style.background = 'blue' ;
             ele[j+1].style.background = 'blue' ;
             if(parseInt(ele[j].style.height) > parseInt(ele[j+1].style.height))
@@ -31,13 +29,12 @@ async function bubble()
     }
     for(let i = 0 ; i < ele.length ; i++)
     {
-        ele[i].style.background = 'green' ;
+        ele[i].style.background = 'cyan' ;
         
         await waitforme(delay) ;
     }
 }
 
-// console.log("reached here") ;
 const bubbleSortBtn = document.querySelector(".bubbleSort") ;
 bubbleSortBtn.addEventListener('click' , async function()
 {
--- insertionSort.js
+++ insertionSort.js
@@ -4,27 +4,6 @@ async function insertion()
     ele[0].style.background = 'green';
     for(let i = 1 ; i < ele.length ; i++)
     {
-        // for(let j = i ; j > 0 ; j--)
-        // {
-        //     ele[j].style.background = 'blue' ;
-        //     ele[j-1].style.background = 'blue' ; 
-        //     if(parseInt(ele[j].style.height) < parseInt(ele[j-1].style.height))
-        //     {
-        //         await waitforme(delay) ;
-        //         swap(ele[j] , ele[j-1]) ;
-        //     }
-        //     // else
-        //     // {
-        //     //     break ;
-        //     // }
-        //     ele[j].style.background = 'cyan' ;
-        //     ele[j-1].style.background = 'cyan' ;
-        //     for(let k = i; k >= 0; k--){
-        //         ele[k].style.background = 'green';
-        //     }
-        // }
-        // ele[i-1].style.background = 'green' ;
-
         let j = i - 1;
         let key = ele[i].style.height;
         ele[i].style.background = 'blue';
@@ -33,6 +12,7 @@ async function insertion()
 
         while(j >= 0 && (parseInt(ele[j].style.height) > parseInt(key))){
             console.log('In while loop');
+            ele[i].style.background = 'blue';
             ele[j].style.background = 'blue';
             ele[j + 1].style.height = ele[j].style.height;
             j--;
